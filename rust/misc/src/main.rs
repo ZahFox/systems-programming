e5b6cb18fa1ca7b74929783f1332ab6f721ae033
zahfox <ZahFoxTales@gmail.com>
2019-12-31 00:11:54 -0600

    feat(rust): added a project for misc code

---
+++ rust/misc/src/main.rs
@@ -0,0 +1,53 @@
+fn main() {
+    for val in vec![1, 3, 4].into_iter().map(|x| x + 1).filter(|x| x > &2) {
+        println!("{}", val);
+    }
+
+    for val in Counter::new() {
+        println!("{}", val);
+    }
+}
+
+struct Counter {
+    count: u32,
+}
+
+impl Counter {
+    fn new() -> Counter {
+        Counter { count: 0 }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.count += 1;
+
+        if self.count < 6 {
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
+#[test]
+fn iterator_sum() {
+    let v1 = vec![1, 2, 3];
+    let v1_iter = v1.iter();
+    let total: i32 = v1_iter.sum();
+    assert_eq!(total, 6);
+}
+
+#[test]
+fn calling_next_directly() {
+    let mut counter = Counter::new();
+
+    assert_eq!(counter.next(), Some(1));
+    assert_eq!(counter.next(), Some(2));
+    assert_eq!(counter.next(), Some(3));
+    assert_eq!(counter.next(), Some(4));
+    assert_eq!(counter.next(), Some(5));
+    assert_eq!(counter.next(), None);
+}
